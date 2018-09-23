e8f33169a0d2296d24e44181c2f9949b2e0234b9
Kirill <vsegda_online_007@bk.ru>
2018-09-23 08:47:56 +0300

    Update and rename omni_hct6753_65u_m0.mk to omni_power.mk

--- omni_power.mk
+++ omni_power.mk
@@ -1,7 +1,7 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := hct6753_65u_m0
+PRODUCT_RELEASE_NAME := power
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -14,8 +14,8 @@ PRODUCT_PACKAGES += \
 	charger
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hct6753_65u_m0
-PRODUCT_NAME := omni_hct6753_65u_m0
+PRODUCT_DEVICE := power
+PRODUCT_NAME := omni_power
 PRODUCT_BRAND := Ulefone
 PRODUCT_MODEL := Ulefone Power
 PRODUCT_MANUFACTURER := Ulefone
